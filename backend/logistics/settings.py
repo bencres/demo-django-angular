379e6d4872a43262a6c92aa8eeb99701a7446096
Ben Cressman <ben@utdallas.edu>
2026-02-22 13:56:50 -0600

    chore: add backend dockerfile

--- backend/logistics/settings.py
+++ backend/logistics/settings.py
@@ -26,7 +26,7 @@ ROOT_URLCONF = 'logistics.urls'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'data' / 'db.sqlite3',
     }
 }
 

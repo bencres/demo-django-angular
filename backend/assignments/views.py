bde946e427538209605885011535a0eff6f4764f
Ben Cressman <ben@utdallas.edu>
2026-02-22 13:29:47 -0600

    fix: accept partial data on put requests

--- backend/assignments/views.py
+++ backend/assignments/views.py
@@ -8,8 +8,16 @@ class AssignmentViewSet(viewsets.ModelViewSet):
     serializer_class = AssignmentSerializer
     http_method_names = ['get', 'post', 'put', 'head', 'options']
 
+    def update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return super().update(request, *args, **kwargs)
+
 
 class DriverViewSet(viewsets.ModelViewSet):
     queryset = Driver.objects.all()
     serializer_class = DriverSerializer
     http_method_names = ['get', 'post', 'put', 'head', 'options']
+
+    def update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        return super().update(request, *args, **kwargs)
